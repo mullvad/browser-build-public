a42127143a74816803de28e01a787e8fff90f494
Magnus Lindstrom <magnus.lindstrom@mullvad.net>
2025-05-20 16:24:18 +0200

    make sure asterisk is placed outside of quotes

--- scripts/build_mullvadbrowser.sh
+++ scripts/build_mullvadbrowser.sh
@@ -8,6 +8,8 @@ make mullvadbrowser-"$RELEASE" && make mullvadbrowser-incrementals-"$RELEASE"
 
 # build artifacts are stored under a subdir without the "tbb-" or "mb-" prefix
 # of TAG, so ignore the first 3 characters in the case of mb-
-cp mullvadbrowser/"$RELEASE"/unsigned/"${TAG:3}"/*.txt ../checksums
+tag_version="${TAG:3}"
 
-rsync --mkpath "$GITHUB_WORKSPACE/tor-browser-build/mullvadbrowser/$RELEASE/unsigned/${TAG:3}/*.txt" cdn1:/mullvadbrowser/"$TAG"/
+cp mullvadbrowser/"$RELEASE"/unsigned/"$tag_version"/*.txt ../checksums
+
+rsync --mkpath "$GITHUB_WORKSPACE/tor-browser-build/mullvadbrowser/$RELEASE/unsigned/$tag_version/"*.txt cdn1:/mullvadbrowser/"$tag_version"/
--- scripts/build_torbrowser.sh
+++ scripts/build_torbrowser.sh
@@ -8,6 +8,8 @@ make torbrowser-"$RELEASE" && make torbrowser-incrementals-"$RELEASE"
 
 # build artifacts are stored under a subdir without the "tbb-" or "mb-" prefix
 # of TAG, so ignore the first 4 characters in the case of tbb-
-cp torbrowser/"$RELEASE"/unsigned/"${TAG:4}"/*.txt ../checksums
+tag_version="${TAG:4}"
 
-rsync --mkpath "$GITHUB_WORKSPACE/tor-browser-build/torbrowser/$RELEASE/unsigned/${TAG:4}/*.txt" cdn1:/torbrowser/"$TAG"/
+cp torbrowser/"$RELEASE"/unsigned/"$tag_version"/*.txt ../checksums
+
+rsync --mkpath "$GITHUB_WORKSPACE/tor-browser-build/torbrowser/$RELEASE/unsigned/$tag_version/"*.txt cdn1:/torbrowser/"$tag_version"/
